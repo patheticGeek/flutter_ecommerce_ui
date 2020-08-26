c54ddabf57d2a28c49083863058dc6f1430f9947
Pathetic Geek <geekpathetic@gmail.com>
2020-08-26 14:44:04 +0530

    fogot password screen fix

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_ecommerce_ui/screens/splash/splash_screen.dart';
+import 'package:flutter_ecommerce_ui/screens/sign_in/sign_in_screen.dart';
+import 'package:flutter_ecommerce_ui/screens/forgot_password/forgot_password_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   '/': (context) => SplashScreen(),
   SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
 };
